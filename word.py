f8b13e02ad7c6939d1ac947bd9c0c13aa16a76f3
bluestme <47037653+bluestme@users.noreply.github.com>
2020-02-29 22:45:43 -0500

    Add files via upload
    A simple python program that helps you to ricite GRE words

---
+++ word.py
@@ -0,0 +1,21 @@
+import time
+wlist1=open("wordlist2.txt","r",encoding='utf-8')
+docu=wlist1.read()
+docu=docu.split("\n")
+for item in docu:
+    i=0
+    while i<100:
+        print(item)
+        i=i+1
+    time.sleep(9)
+wlist1.close()
+wlist2=open("wordlist.txt","r",encoding='utf-8')
+docu2=wlist2.read()
+docu2=docu2.split("\n")
+for item in docu2:
+    i=0
+    while i<100:
+        print(item)
+        i=i+1
+    time.sleep(15)
+wlist2.close()
